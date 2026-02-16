20d730338311edc6e13a4079d95116955d955b73
JP-7808 <jayp7808@gmail.com>
2026-02-16 06:55:29 +0530

    cors updated

--- server.js
+++ server.js
@@ -246,7 +246,12 @@ app.use(helmet({
 }));
 
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: [
+    process.env.FRONTEND_URL,
+    "http://localhost:5173",
+    "https://pariyatan.com",
+    "https://www.pariyatan.com"
+  ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'Cookie', 'Accept']
